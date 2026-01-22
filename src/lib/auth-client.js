bdb95bac16beef428aeacb16ad9e2213c01dc250
fardeenanwar451 <fardeenanwar45@gmail.com>
2026-01-23 02:43:18 +0530

    Refactor auth-client.js for better environment handling

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -3,8 +3,19 @@
 //     baseURL: "http://localhost:3000"
 // })
 
-import { createAuthClient } from "better-auth/react"
+// import { createAuthClient } from "better-auth/react"
 
-export const {signIn , signUp , useSession , signOut} = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000"
-})
+// export const {signIn , signUp , useSession , signOut} = createAuthClient({
+//     baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL || "http://localhost:3000"
+// })
+
+import { createAuthClient } from "better-auth/react";
+
+export const {
+  signIn,
+  signUp,
+  useSession,
+  signOut,
+} = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL!,
+});
